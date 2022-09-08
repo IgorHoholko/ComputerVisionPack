94c769665774d8da7a3d33d7c0f4dee594596319
Ihar Khakholka <igor.xoxolko@gmail.com>
2022-09-08 10:55:23 +0800

    +add CheckBox support for edit menu

--- src/gui/edit_panel.cpp
+++ src/gui/edit_panel.cpp
@@ -8,7 +8,7 @@
 namespace cvp::gui {
 
     EditPanel::EditPanel(QWidget* parent)
-        : QDialog(parent){
+        : QDialog(parent) {
 
         //        this->resize(300, 200);
         _main_layout = new QVBoxLayout(this);
@@ -29,16 +29,16 @@ namespace cvp::gui {
         return _apply_button;
     }
 
-    void EditPanel::addSlider(Qt::Orientation orientation, int min, int max, const QString& desc, const std::string& key) {
+    void EditPanel::addSlider(const std::string& key, Qt::Orientation orientation, int min, int max, const QString& desc) {
         auto* slider = new QSlider(orientation, this);
         int   value  = min + (max - min) / 2;
 
         slider->setRange(min, max);
         slider->setValue(value);
         slider->setMinimumWidth(250);
-        connect(slider, &QSlider::sliderMoved, [&](int value) {QToolTip::showText(QCursor::pos(), QString("%1").arg(value), nullptr); } );
+        connect(slider, &QSlider::sliderMoved, [&](int value) { QToolTip::showText(QCursor::pos(), QString("%1").arg(value), nullptr); });
 
-        auto* label_desc = new QLabel(desc );
+        auto* label_desc = new QLabel(desc);
 
         _form_layout->addRow(label_desc, slider);
 
@@ -46,36 +46,60 @@ namespace cvp::gui {
         connect(slider, &QSlider::valueChanged, [this, key](int value) { _params[key] = value; });
     }
 
-        void EditPanel::addDoubleSpinBox(double min, double max, double step, const QString& desc, const std::string& key) {
-            auto spin_box = new QDoubleSpinBox(this);
-            double  value  = min + (max - min) / 2.;
+    void EditPanel::addDoubleSpinBox(const std::string& key, double min, double max, double step, const QString& desc) {
+        auto   spin_box = new QDoubleSpinBox(this);
+        double value    = min + (max - min) / 2.;
 
-            spin_box->setRange(min, max);
-            spin_box->setValue(value);
-            spin_box->setSingleStep(step);
+        spin_box->setRange(min, max);
+        spin_box->setValue(value);
+        spin_box->setSingleStep(step);
 
-            auto* label_desc = new QLabel(desc );
-//            label_desc->setStyleSheet("font-weight: bold");
+        auto* label_desc = new QLabel(desc);
 
-            _form_layout->addRow(label_desc, spin_box);
+        _form_layout->addRow(label_desc, spin_box);
 
-            _params[key] = value;
-            connect(spin_box, &QDoubleSpinBox::valueChanged, [this, key](int value) { _params[key] = value; });
-        }
+        _params[key] = value;
+        connect(spin_box, &QDoubleSpinBox::valueChanged, [this, key](int value) { _params[key] = value; });
+    }
 
-        void EditPanel::addIntSpinBox(int min, int max, int step, const QString& desc, const std::string& key) {
-        }
+    void EditPanel::addIntSpinBox(const std::string& key, int min, int max, int step, const QString& desc) {
+        auto   spin_box = new QSpinBox(this);
+        int value    = min + (max - min) / 2;
+
+        spin_box->setRange(min, max);
+        spin_box->setValue(value);
+        spin_box->setSingleStep(step);
+
+        auto* label_desc = new QLabel(desc);
+
+        _form_layout->addRow(label_desc, spin_box);
+
+        _params[key] = value;
+        connect(spin_box, &QSpinBox::valueChanged, [this, key](int value) { _params[key] = value; });
+    }
+
+    void EditPanel::addCheckBox(const std::string& key, bool checked, const QString& desc){
+        auto* check_box = new QCheckBox("Box Check", this);
+
+        _form_layout->addRow(check_box);
+
+        _params[key] = check_box->isChecked();
+        connect(check_box, &QCheckBox::stateChanged, [this, key, check_box](bool value) { _params[key] = check_box->isChecked(); });
+    }
 
 
-        int EditPanel::getSliderValue(const std::string& key) {
-            return std::any_cast<int>(_params[key]);
-        }
-        double EditPanel::getDoubleSpinBoxValue(const std::string& key) {
-            return std::any_cast<double>(_params[key]);
-        }
-        int EditPanel::getIntSpinBoxValue(const std::string& key) {
-            return std::any_cast<int>(_params[key]);
-        }
+    int EditPanel::getSliderValue(const std::string& key) {
+        return getParam<int>(key);
+    }
+    double EditPanel::getDoubleSpinBoxValue(const std::string& key) {
+        return getParam<double>(key);
+    }
+    int EditPanel::getIntSpinBoxValue(const std::string& key) {
+        return getParam<int>(key);
+    }
+    bool EditPanel::getCheckBoxValue(const std::string& key) {
+        return getParam<bool>(key);
+    }
 
 
 }// namespace cvp::gui
--- src/gui/edit_panel.h
+++ src/gui/edit_panel.h
@@ -6,12 +6,14 @@
 #define COMPUTERVISIONPACK_EDIT_PANEL_H
 
 
+#include <QCheckBox>
 #include <QDialog>
-#include <QPushButton>
 #include <QDoubleSpinBox>
-#include <QSlider>
 #include <QFormLayout>
 #include <QLabel>
+#include <QPushButton>
+#include <QSlider>
+#include <QSpinBox>
 #include <QToolTip>
 
 #include "common/common.h"
@@ -26,31 +28,35 @@ namespace cvp::gui {
         explicit EditPanel(QWidget* parent = 0);
 
         QPushButton* addApplyButton();
-//        QPushButton* getAppplyButton(){ return _apply_button; };
+        //        QPushButton* getAppplyButton(){ return _apply_button; };
 
-        void addSlider(Qt::Orientation orientation, int min, int max, const QString& desc, const std::string& key);
-        void addDoubleSpinBox(double min, double max, double step, const QString& desc, const std::string& key);
-        void addIntSpinBox(int min, int max, int step, const QString& desc, const std::string& key);
-//
-        int getSliderValue(const std::string& key);
+        void addSlider(const std::string& key, Qt::Orientation orientation, int min, int max, const QString& desc);
+        void addDoubleSpinBox(const std::string& key, double min, double max, double step, const QString& desc);
+        void addIntSpinBox(const std::string& key, int min, int max, int step, const QString& desc);
+        void addCheckBox(const std::string& key, bool checked, const QString& desc);
+        //
+        int    getSliderValue(const std::string& key);
         double getDoubleSpinBoxValue(const std::string& key);
-        int getIntSpinBoxValue(const std::string& key);
+        int    getIntSpinBoxValue(const std::string& key);
+        bool   getCheckBoxValue(const std::string& key);
+
+        template<class Type>
+        Type getParam(const std::string& key) {
+            return std::any_cast<Type>(_params[key]);
+        };
 
         ParamsDict getCurrentParams() const { return _params; };
 
     private:
-
         QVBoxLayout* _main_layout;
         QFormLayout* _form_layout;
         QPushButton* _apply_button;
 
         ParamsDict _params;
-
-
     };
 
 
-};// namespace CVP::gui
+};// namespace cvp::gui
 
 
 #endif//COMPUTERVISIONPACK_EDIT_PANEL_H
