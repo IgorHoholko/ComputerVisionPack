f2c07f02858b370dcb094d30bdf19195fac789df
Ihar Khakholka <igor.xoxolko@gmail.com>
2022-09-11 08:42:14 +0800

    +add models factory | \\\\

---
+++ src/dl_models/models_factory.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Игорь Хохолко on 11.09.22.
+//
+
+#include "models_factory.h"
+
+#include "dl_models/detection/anchor_ssd.h"
+
+namespace cvp{
+
+    BaseDetectorUPtr ModelsFactory::getDetector(const std::string & graph_name, const std::string& resources_path, ModelBackend backend,
+                                               float conf_threshold, bool smart_pad_resize, size_t smart_pad_resize_padding, bool allow_upscale) {
+
+        if (graph_name == "retina_resnet50_sml_480x640") {
+            auto prior_box_params = PriorBoxParams{  {{16,32},{64,128},{256,512}},  {8,16,32}  };
+            std::vector<const char*> output_names = {"bbox_regressions", "classifications", "ldm_regressions"};
+            conf_threshold = (conf_threshold < 0.f) ? 0.95f : conf_threshold;
+            auto settings = DetectorSettings { 640, 480, conf_threshold, {-2.1179, -2.0357, -1.8044}, {2.2489, 2.4286, 2.6400},
+                                             TensorFormatType::NCHW, cv::InterpolationFlags::INTER_LINEAR, smart_pad_resize,
+                                             smart_pad_resize_padding, allow_upscale, output_names};
+            settings.prior_box_params = std::move( prior_box_params ) ;
+//            LOG(Debug, "\tModelsFactory::getDetector: " << "Loading " << graph_name << " detector with <conf_threshold> = " << conf_threshold);
+            return std::make_unique<AnchorSSD>(resources_path, graph_name, backend, settings);
+        }
+
+        std::stringstream msg;
+        msg << "No such Detector!"
+            << " (" << graph_name << ")";
+        throw std::invalid_argument(msg.str().c_str());
+    }
+
+}
---
+++ src/dl_models/models_factory.h
@@ -0,0 +1,23 @@
+//
+// Created by Игорь Хохолко on 11.09.22.
+//
+
+#ifndef COMPUTERVISIONPACK_MODELS_FACTORY_H
+#define COMPUTERVISIONPACK_MODELS_FACTORY_H
+
+#include "dl_models/detection/base_detector.h"
+
+
+namespace cvp {
+
+    class ModelsFactory {
+    public:
+        BaseDetectorUPtr getDetector(const std::string& graph_name, const std::string& resources_path, ModelBackend backend, float conf_threshold = -1.f,
+                                     bool smart_pad_resize = false, size_t smart_pad_resize_padding = 0, bool allow_upscale = true);
+    };
+
+    using ModelsFactoryPtr = std::shared_ptr<ModelsFactory>;
+}// namespace cvp
+
+
+#endif//COMPUTERVISIONPACK_MODELS_FACTORY_H
